e548b8bafec953a128bdaa6d6b98cea60f11613d
Arunsankar S <arun.sankars@u-next.com>
2023-07-03 16:15:57 +0530

    Pass custom data using props in Actions. Update state data in reducer with data

---
+++ src/app/counter/custom-counter-input/custom-counter-input.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { CounterState } from '../state/counter.state';
+import { customIncrement } from '../state/counter.actions';
+
+@Component({
+  selector: 'app-custom-counter-input',
+  templateUrl: './custom-counter-input.component.html',
+  styleUrls: [ './custom-counter-input.component.scss' ]
+})
+export class CustomCounterInputComponent {
+
+  value = 0;
+
+  constructor(private store: Store<{ counter: CounterState }>) {
+  }
+
+  onAdd() {
+    this.store.dispatch(customIncrement({ count: +this.value }));
+  }
+
+}
