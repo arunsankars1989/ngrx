ea036ef8d376a3fd031123dc52f2f07e249afcf6
Arunsankar S <arun.sankars@u-next.com>
2023-07-07 18:29:18 +0530

    Added authguard to the posts url

---
+++ src/app/services/auth.guard.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { map, Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { AppState } from '../state/app.state';
+import { isAuthenticated } from '../auth/state/auth.selector';
+
+@Injectable({ providedIn: 'root' })
+export class AuthGuard {
+
+  constructor(
+    private store: Store<AppState>,
+    private router: Router
+  ) {
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot):
+    boolean
+    | UrlTree
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree> {
+    console.log('came to canActivate');
+    return this.store.select(isAuthenticated).pipe(
+      map((authenticate) => {
+        if (!authenticate) {
+          return this.router.createUrlTree([ 'auth' ]);
+        }
+        return true;
+      })
+    );
+  }
+
+}
